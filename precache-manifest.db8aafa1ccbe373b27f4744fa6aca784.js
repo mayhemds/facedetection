32a75dfc1b2f8be86cd1763104ea636a36132818
Tyrone May <tyronemay@Tyrones-MacBook-Pro.local>
2019-08-21 16:46:01 +0100

    Updates

---
+++ precache-manifest.db8aafa1ccbe373b27f4744fa6aca784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7a705e3082f1ff4128283ea0a9caa6",
+    "url": "/facedetection/index.html"
+  },
+  {
+    "revision": "69e457e2ede8e5ef1403",
+    "url": "/facedetection/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "67e300185edea75e3966",
+    "url": "/facedetection/static/css/main.3909b00c.chunk.css"
+  },
+  {
+    "revision": "69e457e2ede8e5ef1403",
+    "url": "/facedetection/static/js/2.db7ce4e3.chunk.js"
+  },
+  {
+    "revision": "67e300185edea75e3966",
+    "url": "/facedetection/static/js/main.e3eab340.chunk.js"
+  },
+  {
+    "revision": "58aff6f9e26a5fed485f",
+    "url": "/facedetection/static/js/runtime~main.8556d5a5.js"
+  }
+]);
